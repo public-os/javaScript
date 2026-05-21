1458d036d65861687edd3cf719824d8c54072032
Tony <z999zon@gmail.com>
2026-05-21 08:50:28 +0530

    first commit

---
+++ Arrow_Function.js
@@ -0,0 +1,12 @@
+// const functionName = (parameters) => {
+//   // function body
+// };
+
+const add = (a, b) => {
+  return a + b;
+};
+
+const multiply = (a, b) => a * b;
+
+console.log(multiply(3, 4));
+console.log(add(2, 3));
---
+++ Arrow_Function1.js
@@ -0,0 +1,12 @@
+const subtract = () => 789 - 456;
+
+const divide = () => 678 / 8;
+
+const square = () => 89*89;
+
+const isEvenOrOdd = () => (256789%2===0 ? "Even" : "Odd");
+
+console.log("Subtraction result:", subtract());
+console.log("Division result:", divide()); 
+console.log("Square result:", square()); 
+console.log("256789 is:", isEvenOrOdd()); 
